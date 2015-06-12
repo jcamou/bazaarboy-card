a75dec58b3b3eca1f42413e66a03639b3930bbb4
José Camou <jccamou@gmail.com>
2015-06-12 01:18:47 -0500

    Added project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+});
+
+
+function ExampleJS(){
+    var jFirst = document.getElementById("name-on-card").value;
+    var jLast = document.getElementById("cc-number").value;
+    alert("Your name is: " + jFirst + " " + jLast);
+ };
